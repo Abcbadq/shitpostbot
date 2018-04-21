c4011999ce4bf0d089d1c7d41ec9fca2be7faa5a
Abcbadq <32344614+Abcbadq@users.noreply.github.com>
2018-04-21 12:18:03 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const Discord = require('discord.js')
+const config = require('./config.json')
+const snekfetch = require('snekfetch')
+
+const redditurl = "https://www.reddit.com/.json"
+const badsubs = [
+    "PrequelMemes",
+    "polandball",
+    "terriblefacebookmemes",
+    "MemeEconomy",
+    "Dota2",
+    "aww",
+    "SequelMemes",
+    "funny",
+    "me_irl",
+    "meirl",
+    "BikiniBottomTwitter"
+]
+
+const client = new Discord.Client({
+    disableEveryone:true,
+    disableEvents:['TYPING_START']
+})
+
+
+process.on('exit', () => {
+    client.destroy()
+})
+
+client.on('ready',() => {
+    setInterval(async function(){
+        console.log("shitpost activated")
+        var children = await snekfetch.get(redditurl).then(r => r.body.data.children)
+
+        function geturl(child) {
+            if (typeof child.data === 'undefined' ||
+                typeof child.data.preview === 'undefined' ||
+                typeof child.data.preview.images === 'undefined' ||
+                typeof child.data.preview.images[0] === 'undefined' ||
+                typeof child.data.preview.images[0].source === 'undefined' ||
+                typeof child.data.preview.images[0].source.url === 'undefined'){
+                    return undefined
+            }
+            return child.data.preview.images[0].source.url
+        }
+
+        children = children.filter(x => (badsubs.indexOf(x.data.subreddit) > -1))
+                    .map(x => x = geturl(x))
+                    .filter(x => x)
+        
+        if(children.length > 0){
+            console.log(children[Math.floor(Math.random()*children.length)])
+            client.channels.get(config.channelid).send(children[Math.floor(Math.random()*children.length)])
+        }
+    },parseInt((Math.random()+1)*config.timer))
+})
+
+async function shitpost(){
+    
+}
+
+client.login(config.token)
