bd559ac68307d35b687ce1a9ee122bb88b8d5dff
Abcbadq <32344614+Abcbadq@users.noreply.github.com>
2018-05-11 22:19:51 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,22 +2,6 @@ const Discord = require('discord.js')
 const config = require('./config.json')
 const snekfetch = require('snekfetch')
 
-const redditurl = "https://www.reddit.com/.json"
-const badsubs = [
-    "PrequelMemes",
-    "polandball",
-    "terriblefacebookmemes",
-    "MemeEconomy",
-    "Dota2",
-    "aww",
-    "SequelMemes",
-    "funny",
-    "me_irl",
-    "meirl",
-    "BikiniBottomTwitter",
-    "surrealmemes"
-]
-
 const client = new Discord.Client({
     disableEveryone:true,
     disableEvents:['TYPING_START']
@@ -28,33 +12,48 @@ process.on('exit', () => {
 })
 
 client.on('ready',() => {
+	var date = new Date()
+	console.log(`Shitpost-Bot activated on ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+	
     var interval = setInterval(async function() {
-        console.log("shitpost activated")
-        var children = await snekfetch.get(redditurl).then(r => r.body.data.children)
+		var date = new Date()
+        console.log(`Shitposting on ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+        var pool = [] 
 
-        function geturl(child) {
-            if (typeof child.data === 'undefined' ||
-                typeof child.data.preview === 'undefined' ||
-                typeof child.data.preview.images === 'undefined' ||
-                typeof child.data.preview.images[0] === 'undefined' ||
-                typeof child.data.preview.images[0].source === 'undefined' ||
-                typeof child.data.preview.images[0].source.url === 'undefined'){
-                    return undefined
+        for(subreddit in config.badsubs){
+
+            var subChildren = await snekfetch.get(`${config.redditheader}${subreddit}/.json`).then(r => r.body.data.children)
+
+            subChildren = subChildren
+                .filter(x => (badsubs.indexOf(x.data.subreddit) > -1))
+                .map(x => x = getSubUrl(x))
+                .filter(x => x)
+            
+            if(subChildren.length > 0){
+                pool.push(subChildren[Math.floor(Math.random()*subChildren.length)])
             }
-            if (child.data.preview.images[0].variants.length > 0 && child.data.preview.images[0].variants.indexOf('gif') > (-1)){
-                return child.data.preview.images[0].variants.gif.source.url
-            }
-            return child.data.preview.images[0].source.url
         }
 
-        children = children.filter(x => (badsubs.indexOf(x.data.subreddit) > -1))
-                    .map(x => x = geturl(x))
-                    .filter(x => x)
-        
-        if(children.length > 0){
-            console.log(children[Math.floor(Math.random()*children.length)])
-            client.channels.get(config.channelid).send(children[Math.floor(Math.random()*children.length)])
+        if(pool.length > 0){
+			var picked_link = pool[Math.floor(Math.random()*pool.length)]
+            console.log(picked_link)
+            client.channels.get(config.channelid).send(picked_link)
         }},config.timer)
 })
 
 client.login(config.token)
+
+function getSubUrl(child) {
+    if (typeof child.data === 'undefined' ||
+        typeof child.data.preview === 'undefined' ||
+        typeof child.data.preview.images === 'undefined' ||
+        typeof child.data.preview.images[0] === 'undefined' ||
+        typeof child.data.preview.images[0].source === 'undefined' ||
+        typeof child.data.preview.images[0].source.url === 'undefined'){
+            return undefined
+    }
+    if (child.data.preview.images[0].variants.length > 0 && child.data.preview.images[0].variants.indexOf('gif') > (-1)){
+        return child.data.preview.images[0].variants.gif.source.url
+    }
+    return child.data.preview.images[0].source.url
+}
