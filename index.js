268cc56e98add8bc05647b640fb2908bbe564799
Abcbadq <32344614+Abcbadq@users.noreply.github.com>
2018-05-14 22:31:55 -0700

    Changed index
    Made index more readable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 const config = require('./config.json')
 const snekfetch = require('snekfetch')
+const winston = require('winston')
 
 const client = new Discord.Client({
     disableEveryone:true,
@@ -12,48 +13,74 @@ process.on('exit', () => {
 })
 
 client.on('ready',() => {
-	var date = new Date()
-	console.log(`Shitpost-Bot activated on ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
-	
-    var interval = setInterval(async function() {
-		var date = new Date()
-        console.log(`Shitposting on ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
-        var pool = [] 
-
-        for(subreddit in config.badsubs){
-
-            var subChildren = await snekfetch.get(`${config.redditheader}${subreddit}/.json`).then(r => r.body.data.children)
-
-            subChildren = subChildren
-                .filter(x => (badsubs.indexOf(x.data.subreddit) > -1))
-                .map(x => x = getSubUrl(x))
-                .filter(x => x)
-            
-            if(subChildren.length > 0){
-                pool.push(subChildren[Math.floor(Math.random()*subChildren.length)])
-            }
-        }
-
-        if(pool.length > 0){
-			var picked_link = pool[Math.floor(Math.random()*pool.length)]
-            console.log(picked_link)
-            client.channels.get(config.channelid).send(picked_link)
-        }},config.timer)
+    var date = new Date()
+	winston.info(`Shitpost-Bot ready on ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+    
+    var timedPost = setInterval(function(){sendImage(getImage())},config.timer)
 })
 
-client.login(config.token)
+async function getImage(){
+    var date = new Date()
+    winston.info(`Shitpost-Bot pulling pictures on ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+    
+    var pool = []
 
-function getSubUrl(child) {
+    for(var i=0;i<config.subs.length;i++){
+        const currentsub = config.subs[i]
+        try{
+            var subchildren = await getSubBody(`${config.redditheader}${currentsub}/top/.json`)
+            subchildren = subchildren
+                    .map(x => x = getPictureUrl(x))
+                    .filter(x => x)
+            if(subchildren.length > 0){
+                pool.push(pickOne(subchildren))
+            }
+        }
+        catch(err){
+            winston.error(`${currentsub} has caused an error:${err.message}`)
+        }
+    }
+    winston.info(pool)
+    if(pool.length > 0){
+        return pool[Math.floor(Math.random()*pool.length)]
+    }
+    return null
+}
+
+async function getSubBody(url){
+    return await snekfetch.get(url).then(r => r.body.data.children)
+}
+
+function getPictureUrl(child){
     if (typeof child.data === 'undefined' ||
         typeof child.data.preview === 'undefined' ||
         typeof child.data.preview.images === 'undefined' ||
         typeof child.data.preview.images[0] === 'undefined' ||
         typeof child.data.preview.images[0].source === 'undefined' ||
         typeof child.data.preview.images[0].source.url === 'undefined'){
-            return undefined
+        return undefined
     }
-    if (child.data.preview.images[0].variants.length > 0 && child.data.preview.images[0].variants.indexOf('gif') > (-1)){
+    if(child.data.preview.images[0].variants.length > 0 && child.data.prevgiew.images[0].variants.indexOf('gif') > -1){
         return child.data.preview.images[0].variants.gif.source.url
     }
     return child.data.preview.images[0].source.url
 }
+
+function pickOne(array){
+    for(var i=0;i<array.length;i++){
+        if(Math.random()>.5){
+            return array[i]
+        }
+    }
+    return array.pop()
+}
+
+function sendImage(url){
+    if(url !== null){
+        var date = new Date()
+        winston.info(`Shitpost-Bot activated on ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+        client.channels.get(config.channelid).send(url)
+    }
+}
+
+client.login(config.token)
