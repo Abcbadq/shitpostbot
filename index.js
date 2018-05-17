8d484a5402e749af7100fb0ae13a69125ca39b46
Abcbadq <32344614+Abcbadq@users.noreply.github.com>
2018-05-16 23:45:20 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const config = require('./config.json')
 const snekfetch = require('snekfetch')
 const winston = require('winston')
 
+const redditheader = "https://www.reddit.com/r/"
+
 const client = new Discord.Client({
     disableEveryone:true,
     disableEvents:['TYPING_START']
@@ -16,31 +18,43 @@ client.on('ready',() => {
     var date = new Date()
 	winston.info(`Shitpost-Bot ready on ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
     
-    var timedPost = setInterval(function(){sendImage(getImage())},config.timer)
+    var timedPost = setInterval(function(){sendImage()},config.timer)
 })
+
+async function sendImage(u){
+    var url = await getImage()
+    if(url !== null){
+        var date = new Date()
+        winston.info(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}-Activated`)
+        client.channels.get(config.channelid).send(url)
+    }
+    else{
+        var date = new Date()
+        winston.info(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}-No images found`)
+    }
+}
 
 async function getImage(){
     var date = new Date()
-    winston.info(`Shitpost-Bot pulling pictures on ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+    winston.info(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}-Pulling images`)
     
     var pool = []
 
     for(var i=0;i<config.subs.length;i++){
         const currentsub = config.subs[i]
         try{
-            var subchildren = await getSubBody(`${config.redditheader}${currentsub}/top/.json`)
+            var subchildren = await getSubBody(`${redditheader}${currentsub}/top/.json`)
             subchildren = subchildren
-                    .map(x => x = getPictureUrl(x))
+                    .map(x => x = nonAsyncGetPictureUrl(x))
                     .filter(x => x)
             if(subchildren.length > 0){
-                pool.push(pickOne(subchildren))
+                pool.push(nonAsyncPickOne(subchildren))
             }
         }
         catch(err){
             winston.error(`${currentsub} has caused an error:${err.message}`)
         }
     }
-    winston.info(pool)
     if(pool.length > 0){
         return pool[Math.floor(Math.random()*pool.length)]
     }
@@ -51,7 +65,7 @@ async function getSubBody(url){
     return await snekfetch.get(url).then(r => r.body.data.children)
 }
 
-function getPictureUrl(child){
+function nonAnsyncGetPictureUrl(child){
     if (typeof child.data === 'undefined' ||
         typeof child.data.preview === 'undefined' ||
         typeof child.data.preview.images === 'undefined' ||
@@ -66,21 +80,13 @@ function getPictureUrl(child){
     return child.data.preview.images[0].source.url
 }
 
-function pickOne(array){
+function nonAnsyncPickOne(array){
     for(var i=0;i<array.length;i++){
         if(Math.random()>.5){
             return array[i]
         }
     }
     return array.pop()
-}
-
-function sendImage(url){
-    if(url !== null){
-        var date = new Date()
-        winston.info(`Shitpost-Bot activated on ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
-        client.channels.get(config.channelid).send(url)
-    }
 }
 
 client.login(config.token)
