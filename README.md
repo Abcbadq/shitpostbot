9aa5697fdc2852b62ebefc43428c2110223c6f31
Abcbadq <32344614+Abcbadq@users.noreply.github.com>
2018-06-20 13:02:00 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Must have a config.json that contains:
   
   "timer":"-number of milliseconds between posts-",
   
-  "subs":["-insert an array of subreddits that will be picked from"]
+  "admin":["-insert an array of admin id that can use commands"]
   
 }
 
@@ -21,3 +21,11 @@ Must have a config.json that contains:
 On a timer the bot will go to the designated subreddits and pull a random picture file from the top 24 hour posts and put it in an aggregate pool.
 
 A random picture is pulled from the aggregate pool of pictures and posted in a discord text channel.
+
+# Commands
+
+!subs: returns a list of subreddits the picture can choose from
+!add [item]: add a subreddit
+!remove[item]: remove a subreddit
+!help: list commands
+!hockey: post a picture
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js')
-const config = require('./config.json')
 const snekfetch = require('snekfetch')
 const winston = require('winston')
+const fs = require('fs')
+const defaultSubs = require('./default.json')
+const config = require('./config.json')
 
 const redditheader = "https://www.reddit.com/r/"
 
@@ -21,8 +23,67 @@ client.on('ready',() => {
     var timedPost = setInterval(function(){sendImage()},config.timer)
 })
 
-async function sendImage(u){
-    var url = await getImage()
+const juggernaut = "http://i2.kym-cdn.com/photos/images/original/000/456/960/d5e.jpg"
+client.on('message' , msg => {
+    if(msg.author.id == '108454013384544256' && msg.content.toLowerCase().indexOf('jug') >= 0){
+        msg.reply(juggernaut)
+    }
+    if(msg.content.startsWith('!') && config.admins.indexOf(msg.author.id) >= 0 ){
+        var subs = require('./refined.json')
+        if(msg.content.startsWith('!hockey')){
+            sendImage(subs)
+        }
+        if(msg.content.startsWith('!add')){
+            const addCommand = msg.content.split(' ')
+            if(addCommand.length !== 2){
+                msg.reply(`The command comes in the form of !add *sub*`)
+            }
+            else{
+                if(subs.indexOf(addCommand[1]) >= 0){
+                    msg.reply(`Duplicate sub`)
+                }
+                else{
+                    subs.push(addCommand[1])
+                    msg.reply(`Added [${addCommand[1]}]`)
+                }
+            }
+        }
+        else if(msg.content.startsWith('!remove')){
+            const removeCommand = msg.content.split(' ')
+            if(removeCommand.length !== 2){
+                msg.reply(`The command comes in the form of !remove *sub*`)
+            }
+            else{
+                const removeIndex = subs.indexOf(removeCommand[1])
+                if(removeIndex >= 0){
+                    subs.splice(removeIndex,removeIndex + 1)
+                    msg.reply(`Removed [${removeCommand[1]}]`)
+                }
+                else{
+                    msg.reply(`Non-existant sub`)
+                }
+            }
+        }
+        else if(msg.content.startsWith('!subs')){
+            msg.reply(subs)
+        }
+        else if(msg.content.startsWith('!reset')){
+            var subs = defaultSubs
+            msg.reply('Reset subs')
+        }
+        else if(msg.content.startsWith('!save')){
+            saveSubs(subs)
+            msg.reply('Subs saved')
+        }
+        else if(msg.content.startsWith('!command')){
+            msg.reply(['!hockey' , '!add [sub]' , '!remove [sub]' , '!subs' , '!reset' , '!save'])
+        }
+    }
+})
+
+async function sendImage(){
+    var subs = require('./refined.json')
+    var url = await getImage(subs)
     if(url !== null){
         var date = new Date()
         winston.info(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}-Activated`)
@@ -34,14 +95,28 @@ async function sendImage(u){
     }
 }
 
-async function getImage(){
+function saveSubs(subs){
+    fs.writeFile(`./refined.json` , JSON.stringify( subs , null , 4) , (err) => {
+        if(err){
+            var date = new Date()
+            winston.info(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}-Error while writing sub file`)
+            return
+        }
+        else{
+            var date = new Date()
+            winston.info(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}-Wrote sub file`)
+        }
+    })
+}
+
+async function getImage(subs){
     var date = new Date()
     winston.info(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}-Pulling images`)
     
     var pool = []
 
-    for(var i=0;i<config.subs.length;i++){
-        const currentsub = config.subs[i]
+    for(var i=0 ; i < subs.length ; i++){
+        const currentsub = subs[i]
         try{
             var subchildren = await getSubBody(`${redditheader}${currentsub}/top/.json`)
             subchildren = subchildren
@@ -65,7 +140,7 @@ async function getSubBody(url){
     return await snekfetch.get(url).then(r => r.body.data.children)
 }
 
-function nonAnsyncGetPictureUrl(child){
+function nonAsyncGetPictureUrl(child){
     if (typeof child.data === 'undefined' ||
         typeof child.data.preview === 'undefined' ||
         typeof child.data.preview.images === 'undefined' ||
@@ -80,7 +155,7 @@ function nonAnsyncGetPictureUrl(child){
     return child.data.preview.images[0].source.url
 }
 
-function nonAnsyncPickOne(array){
+function nonAsyncPickOne(array){
     for(var i=0;i<array.length;i++){
         if(Math.random()>.5){
             return array[i]
